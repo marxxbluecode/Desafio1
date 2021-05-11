7b2bda99fa365ce06af786c95aa3227e692f4433
marxxcodes <marxx171@gmail.com>
2021-05-10 21:23:57 -0300

    Desafio1 vnw

---
+++ index.js
@@ -0,0 +1,20 @@
+const nome = 'Marx'
+const idade = 23
+const ano_atual = 2021 
+
+console.log( `Meu ano de nascimento é ${ano_atual - idade}`) 
+
+// 122 anos é a maior idade já documentada para uma pessoa na história
+const sou_humano = true
+const inumano = false
+if (idade <= 122)
+    console.log(sou_humano)
+else
+    console.log(inumano)
+
+const number = 232
+const number2 = 2527
+console.log(number + number2)
+
+
+
